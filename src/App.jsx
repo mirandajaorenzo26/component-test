81e63e63183f2efbaa52abea4374132c56d90585
Jao Renzo Miranda <mirandajaorenzo@gmail.com>
2023-05-30 22:56:49 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ClickAndDragToScroll from "./components/ClickAndDragToScroll";
+
+function App() {
+  return (
+    <>
+      <div className="grid min-h-screen items-center bg-slate-400 px-5">
+        <ClickAndDragToScroll />
+      </div>
+    </>
+  );
+}
+
+export default App;
